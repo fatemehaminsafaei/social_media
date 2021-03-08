6698df02ad2eecae2c7859c1de3e9b60bc26e283
fatemeh.as <fatemehaminsafaei@gmail.com>
2021-03-08 18:39:43 +0330

    new edit

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,8 +1,10 @@
 import sys
+
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.db.models import Count
-from django.shortcuts import get_object_or_404, render,redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 from apps.blog.models import Post, Comment, Preference
 from apps.blog.forms import NewCommentForm
@@ -190,3 +192,99 @@ class FollowersListView(ListView):
         data = super().get_context_data(**kwargs)
         data['follow'] = 'followers'
         return data
+
+
+@login_required
+def postpreference(request, postid, userpreference):
+    if request.method == "POST":
+        eachpost = get_object_or_404(Post, id=postid)
+
+        obj = ''
+
+        valueobj = ''
+
+        try:
+            obj = Preference.objects.get(user=request.user, post=eachpost)
+
+            valueobj = obj.value  # value of userpreference
+
+            valueobj = int(valueobj)
+
+            userpreference = int(userpreference)
+
+            if valueobj != userpreference:
+                obj.delete()
+
+                upref = Preference()
+                upref.user = request.user
+
+                upref.post = eachpost
+
+                upref.value = userpreference
+
+                if userpreference == 1 and valueobj != 1:
+                    eachpost.likes += 1
+                    eachpost.dislikes -= 1
+                elif userpreference == 2 and valueobj != 2:
+                    eachpost.dislikes += 1
+                    eachpost.likes -= 1
+
+                upref.save()
+
+                eachpost.save()
+
+                context = {'eachpost': eachpost,
+                           'postid': postid}
+
+                return redirect('blog-home')
+
+            elif valueobj == userpreference:
+                obj.delete()
+
+                if userpreference == 1:
+                    eachpost.likes -= 1
+                elif userpreference == 2:
+                    eachpost.dislikes -= 1
+
+                eachpost.save()
+
+                context = {'eachpost': eachpost,
+                           'postid': postid}
+
+                return redirect('blog-home')
+
+
+
+
+        except Preference.DoesNotExist:
+            upref = Preference()
+
+            upref.user = request.user
+
+            upref.post = eachpost
+
+            upref.value = userpreference
+
+            userpreference = int(userpreference)
+
+            if userpreference == 1:
+                eachpost.likes += 1
+            elif userpreference == 2:
+                eachpost.dislikes += 1
+
+            upref.save()
+
+            eachpost.save()
+
+            context = {'eachpost': eachpost,
+                       'postid': postid}
+
+            return redirect('blog-home')
+
+
+    else:
+        eachpost = get_object_or_404(Post, id=postid)
+        context = {'eachpost': eachpost,
+                   'postid': postid}
+
+        return redirect('blog-home')
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,16 +2,20 @@ from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, 
     PasswordResetConfirmView
 from django.urls import path
 from . import views
+
 # from users import views as users_views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/',PasswordResetView.as_view(template_name='users/password_reset.html'),name='password-reset'),
-    path('password-reset/done', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password-reset-done'),
-    path('password-reset/confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password-reset-done'),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password-reset'),
+    path('password-reset/done', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password-reset-done'),
+    path('password-reset/confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password-reset-done'),
     path('register/', views.register, name='register-users'),
     path('profile/', views.profile, name='profile'),
     path('search/', views.SearchView, name='search'),
 ]
-
